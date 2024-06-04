0b3a33e058cc4fbf42657f3bb493e0b6987b2e9f
mars-86 <hrslash86@gmail.com>
2024-06-04 13:55:54 -0300

    refac: remove helper functions

--- usb.h
+++ usb.h
@@ -7,9 +7,6 @@
 
 typedef int usb_dev_t;
 
-typedef struct usb_parsed_device_descriptor updd_t;
-typedef struct usb_parsed_config_descriptor upcd_t;
-
 int usb_clear_feature(int fd, unsigned short feature_selector, unsigned short w_index);
 int usb_get_configuration(int fd, unsigned char* buff, int* wlen);
 int usb_get_descriptor(int fd, unsigned char type, unsigned char index, unsigned short langid, unsigned char* buff, int* wlen);
@@ -26,10 +23,5 @@ int usb_release_interface(int fd, unsigned short interface);
 int usb_get_driver(int fd, unsigned short interface, char* driver, size_t len);
 int usb_bulk_send(int fd, uint16_t endpoint, void* data, uint32_t len);
 int usb_bulk_recv(int fd, uint16_t endpoint, void* data, uint32_t len);
-void usb_dump_device(int fd, FILE* output);
-int usb_get_string(int fd, unsigned char index, unsigned short langid, char* buff);
-void* parse_descriptor(unsigned char* src);
-void release_descriptor(void* descriptor);
-void parse_config_descriptor(unsigned char* stream);
 
 #endif // __USB_INCLUDED_H__
